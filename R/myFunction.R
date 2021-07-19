29bb11c9f08e941ea0fc44434881d73e90bb571b
Mary Rohrdanz <marohrdanz@mdanderson.org>
2021-07-19 15:40:26 -0500

    Initial commit

---
+++ R/myFunction.R
@@ -0,0 +1,10 @@
+#' Print a welcome message
+#'
+#' This function prints a welcome message
+#'
+#' @param name Name of user
+#' @return null
+#'
+myFunction <- function(name) {
+	message(paste("Hello, ",name,"!"))
+}
